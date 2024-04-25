44f52a6da56767ed2f6d4e1e7cfb39a2599179e8
137pjy <100476142+137pjy@users.noreply.github.com>
2024-04-25 11:01:01 +0900

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -7,6 +7,12 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+# JAR 파일이 존재하는지 확인
+if [ ! -f "$JAR_FILE" ]; then
+  echo "$TIME_NOW > $JAR_FILE 파일이 존재하지 않습니다" >> $DEPLOY_LOG
+  exit 1
+fi
+
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
